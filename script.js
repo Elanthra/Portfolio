8a0ee6a803ef0f0c695c0b52f43ef9dcb2d7fb61
Joel S Manthra <149935283+Elanthra@users.noreply.github.com>
2024-03-13 12:10:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.from(".left",{
+    x:-300,
+    opacity:0,
+    duration:1
+})
+gsap.from(".right",{
+    x:200,
+    opacity:0,
+    duration:1
+})
+gsap.from(".rightSection",{
+    x:200,
+    opacity:0,
+    duration:1
+})
